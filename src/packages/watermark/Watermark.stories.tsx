5818df605e293ef05759f993c81ad4a5ff2b943e
226307 <226307@nbcb.com>
2023-10-19 18:37:12 +0800

    feat:手写doc示例

--- src/packages/watermark/Watermark.stories.tsx
+++ src/packages/watermark/Watermark.stories.tsx
@@ -13,7 +13,7 @@ const meta = {
     layout: 'centered',
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
+  // tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   // argTypes: {
   //   content: { control: 'text' },
