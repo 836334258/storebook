396655cf6f5ed25b14c7e4a9b3f6ca944d0499d0
jinhaitao <836334258@qq.com>
2023-10-25 19:23:42 +0800

    fix : 删除css

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,6 @@
 import type { Preview } from '@storybook/react'
 import '@nutui/nutui-react/dist/style.css'
-import './index.css'
+// import './index.css'
 
 const preview: Preview = {
   parameters: {
