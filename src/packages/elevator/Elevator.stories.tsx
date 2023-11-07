33609fe32587ebd368f3f22f2cb02aac2e901e2a
jinhaitao <836334258@qq.com>
2023-11-07 17:11:50 +0800

    feat: elevator

---
+++ src/packages/elevator/Elevator.stories.tsx
@@ -0,0 +1,201 @@
+import React from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+import Elevator from './index'
+
+const meta = {
+  title: 'Example/Elevator 电梯楼层',
+  component: Elevator,
+  parameters: {
+    layout: 'centered',
+  },
+  render: (args) => {
+    return (
+      <div style={{ height: '100%px', width: '800px' }}>
+        <Elevator {...args} />
+      </div>
+    )
+  },
+} satisfies Meta<typeof Elevator>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Demo1: Story = {
+  name: '基本用法',
+  args: {
+    list: [
+      {
+        title: 'A',
+        list: [
+          {
+            name: '安徽',
+            id: 1,
+          },
+        ],
+      },
+      {
+        title: 'B',
+        list: [
+          {
+            name: '北京',
+            id: 2,
+          },
+        ],
+      },
+      {
+        title: 'G',
+        list: [
+          {
+            name: '广西',
+            id: 3,
+          },
+          {
+            name: '广东',
+            id: 4,
+          },
+        ],
+      },
+      {
+        title: 'H',
+        list: [
+          {
+            name: '湖南',
+            id: 5,
+          },
+          {
+            name: '湖北',
+            id: 6,
+          },
+          {
+            name: '河南',
+            id: 7,
+          },
+        ],
+      },
+    ],
+  },
+}
+
+export const Demo2: Story = {
+  name: '自定义索引',
+  args: {
+    floorKey: 'num',
+    list: [
+      {
+        num: '一',
+        list: [
+          {
+            name: '北京',
+            id: 1,
+          },
+          {
+            name: '上海',
+            id: 2,
+          },
+          {
+            name: '深圳',
+            id: 3,
+          },
+          {
+            name: '广州',
+            id: 4,
+          },
+          {
+            name: '杭州',
+            id: 5,
+          },
+        ],
+      },
+      {
+        num: '二',
+        list: [
+          {
+            name: '成都',
+            id: 6,
+          },
+          {
+            name: '西安',
+            id: 7,
+          },
+          {
+            name: '天津',
+            id: 8,
+          },
+          {
+            name: '武汉',
+            id: 9,
+          },
+          {
+            name: '长沙',
+            id: 10,
+          },
+          {
+            name: '重庆',
+            id: 11,
+          },
+          {
+            name: '苏州',
+            id: 12,
+          },
+          {
+            name: '南京',
+            id: 13,
+          },
+        ],
+      },
+      {
+        num: '三',
+        list: [
+          {
+            name: '西宁',
+            id: 14,
+          },
+          {
+            name: '兰州',
+            id: 15,
+          },
+          {
+            name: '石家庄',
+            id: 16,
+          },
+          {
+            name: '秦皇岛',
+            id: 17,
+          },
+          {
+            name: '大连',
+            id: 18,
+          },
+          {
+            name: '哈尔滨',
+            id: 19,
+          },
+          {
+            name: '长春',
+            id: 20,
+          },
+          {
+            name: '太原',
+            id: 21,
+          },
+        ],
+      },
+    ],
+  },
+}
+
+export const Demo3: Story = {
+  name: '不展示右侧导航',
+  args: {
+    ...Demo1.args,
+    showKeys: false,
+  },
+}
+
+export const Demo4: Story = {
+  name: '吸顶索引',
+  args: {
+    ...Demo1.args,
+    sticky: true,
+  },
+}
--- src/packages/elevator/elevator.tsx
+++ src/packages/elevator/elevator.tsx
@@ -13,14 +13,53 @@ import { BasicComponent, ComponentDefaults } from '@/utils/typings'
 export const elevatorContext = createContext({} as ElevatorData)
 
 export interface ElevatorProps extends BasicComponent {
+  /**
+   * 电梯区域的高度
+   * @default 200px
+   */
   height: number | string
+  /**
+   * 索引 key 值
+   * @default title
+   */
   floorKey: string
+  /**
+   * 索引列表
+   * @default [{id: 0, name: ''}]
+   */
   list: any[]
+  /**
+   * 索引是否吸顶
+   * @default false
+   */
   sticky: boolean
+  /**
+   * 右侧锚点的上下间距
+   * @default 23
+   */
   spaceHeight: number
+  /**
+   * 左侧索引的高度
+   * @default 35
+   */
   titleHeight: number
+  /**
+   * 展示右侧导航
+   * @default true
+   */
   showKeys: boolean
+  /**
+   * 点击内容
+   * @default false
+   * @param key
+   * @param item
+   */
   onItemClick: (key: string, item: ElevatorData) => void
+  /**
+   * 点击索引
+   * @default false
+   * @param key
+   */
   onIndexClick: (key: string) => void
 }
 export const defaultProps = {
