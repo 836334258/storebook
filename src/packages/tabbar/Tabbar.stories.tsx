d8f5c860e0be096ebea8bcce5828af9107042bd5
jinhaitao <836334258@qq.com>
2023-11-15 18:59:27 +0800

    feat: Tabbar 标签栏

--- src/packages/tabbar/Tabbar.stories.tsx
+++ src/packages/tabbar/Tabbar.stories.tsx
@@ -6,23 +6,14 @@ import { Home, Category, Find, Cart, My } from '@nutui/icons-react'
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
-  title: 'Example/Tabbar',
+  title: 'Example/Tabbar 标签栏',
   component: Tabbar,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: 'centered',
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  tags: ['autodocs'],
-  render: ({ ...args }) => (
-    <Tabbar {...args}>
-      <Tabbar.Item title="首页" icon={<Home width={20} height={20} />} />
-      <Tabbar.Item title="分类" icon={<Category width={20} height={20} />} />
-      <Tabbar.Item title="发现" icon={<Find width={20} height={20} />} />
-      <Tabbar.Item title="购物车" icon={<Cart width={20} height={20} />} />
-      <Tabbar.Item title="我的" icon={<My width={20} height={20} />} />
-    </Tabbar>
-  ),
+  // tags: ['autodocs'],,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   // argTypes: {
   //   backgroundColor: { control: 'color' },
@@ -33,4 +24,114 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary: Story = {}
+export const Demo1: Story = {
+  name: '基础用法',
+  render: ({ ...args }) => (
+    <div style={{ height: '50px', width: '500px', position: 'relative' }}>
+      <Tabbar {...args}>
+        <Tabbar.Item title="首页" icon={<Home width={20} height={20} />} />
+        <Tabbar.Item title="分类" icon={<Category width={20} height={20} />} />
+        <Tabbar.Item title="发现" icon={<Find width={20} height={20} />} />
+        <Tabbar.Item title="购物车" icon={<Cart width={20} height={20} />} />
+        <Tabbar.Item title="我的" icon={<My width={20} height={20} />} />
+      </Tabbar>
+    </div>
+  ),
+}
+
+export const Demo2: Story = {
+  name: '自定义选中',
+  render: ({ ...args }) => (
+    <div style={{ height: '50px', width: '500px', position: 'relative' }}>
+      <Tabbar {...args}>
+        <Tabbar.Item title="首页" icon={<Home width={20} height={20} />} />
+        <Tabbar.Item title="分类" icon={<Category width={20} height={20} />} />
+        <Tabbar.Item title="发现" icon={<Find width={20} height={20} />} />
+        <Tabbar.Item title="购物车" icon={<Cart width={20} height={20} />} />
+        <Tabbar.Item title="我的" icon={<My width={20} height={20} />} />
+      </Tabbar>
+    </div>
+  ),
+  args: {
+    value: 2,
+  },
+}
+
+export const Demo3: Story = {
+  name: '只配图标',
+  render: ({ ...args }) => (
+    <div style={{ height: '50px', width: '500px', position: 'relative' }}>
+      <Tabbar {...args}>
+        <Tabbar.Item title="首页" icon={<Home width={20} height={20} />} />
+        <Tabbar.Item title="分类" icon={<Category width={20} height={20} />} />
+        <Tabbar.Item icon={<Find width={20} height={20} />} />
+        <Tabbar.Item title="购物车" icon={<Cart width={20} height={20} />} />
+        <Tabbar.Item title="我的" icon={<My width={20} height={20} />} />
+      </Tabbar>
+    </div>
+  ),
+}
+
+export const Demo4: Story = {
+  name: '无图标',
+  render: ({ ...args }) => (
+    <div style={{ height: '50px', width: '500px', position: 'relative' }}>
+      <Tabbar {...args}>
+        <Tabbar.Item title="首页" />
+        <Tabbar.Item title="分类" />
+        <Tabbar.Item title="发现" />
+        <Tabbar.Item title="购物车" />
+        <Tabbar.Item title="我的" />
+      </Tabbar>
+    </div>
+  ),
+}
+
+export const Demo5: Story = {
+  name: '徽标提示',
+  render: ({ ...args }) => (
+    <div style={{ height: '50px', width: '500px', position: 'relative' }}>
+      <Tabbar {...args}>
+        <Tabbar.Item title="首页" value={11} />
+        <Tabbar.Item title="分类" />
+        <Tabbar.Item title="发现" />
+        <Tabbar.Item title="购物车" />
+        <Tabbar.Item title="我的" />
+      </Tabbar>
+    </div>
+  ),
+}
+
+export const Demo6: Story = {
+  name: '红点',
+  render: ({ ...args }) => (
+    <div style={{ height: '50px', width: '500px', position: 'relative' }}>
+      <Tabbar {...args}>
+        <Tabbar.Item dot title="首页" value={11} />
+        <Tabbar.Item title="分类" />
+        <Tabbar.Item dot title="发现" />
+        <Tabbar.Item title="购物车" />
+        <Tabbar.Item title="我的" />
+      </Tabbar>
+    </div>
+  ),
+}
+
+export const Demo7: Story = {
+  name: '自定义颜色',
+  render: ({ ...args }) => (
+    <div style={{ height: '50px', width: '500px', position: 'relative' }}>
+      <Tabbar {...args}>
+        <Tabbar.Item dot title="首页" value={11} />
+        <Tabbar.Item title="分类" />
+        <Tabbar.Item dot title="发现" />
+        <Tabbar.Item title="购物车" />
+        <Tabbar.Item title="我的" />
+      </Tabbar>
+    </div>
+  ),
+  args: {
+    inactiveColor: '#7d7e80',
+    activeColor: '#1989fa',
+  },
+}
--- src/packages/tabbar/tabbar.tsx
+++ src/packages/tabbar/tabbar.tsx
@@ -5,12 +5,39 @@ import { usePropsValue } from '@/utils/use-props-value'
 import TabbarItem from '../tabbaritem'
 
 export interface TabbarProps extends BasicComponent {
+  /**
+   * 默认选中的标签的索引值
+   * @default 0
+   */
   defaultValue: number
+  /**
+   * 选中的标签的索引值
+   */
   value?: number
+  /**
+   * 是否固定在页面底部，为 true 时默认开启 safeArea
+   * @default false
+   */
   fixed: boolean
+  /**
+   * icon未激活的颜色
+   * @default #7d7e80
+   */
   inactiveColor: string
+  /**
+   * icon激活的颜色
+   * @default #1989fa
+   */
   activeColor: string
+  /**
+   * 是否开启iphone系列全面屏底部安全区适配
+   * @default false
+   */
   safeArea: boolean
+  /**
+   * 切换页签时触发事件
+   * @param value
+   */
   onSwitch: (value: number) => void
 }
 
